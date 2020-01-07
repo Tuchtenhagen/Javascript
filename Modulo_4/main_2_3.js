3b48944be0d441b82ade1d46c154fabc6852ce9e
william xavier <william.10.xavier@hotmail.com>
2020-01-07 16:06:29 -0300

    Adicionado final do exercício, porém foi alterado configuração no axios

--- Modulo_4/main_2_3.js
+++ Modulo_4/main_2_3.js
@@ -1,12 +1,13 @@
 var inputElement = document.querySelector('input');
 var buttonElement = document.querySelector('button');
+var ulElement = document.querySelector('ul');
 
 buttonElement.setAttribute('onclick', 'procurarUsuario()');
 
 
 function procurarUsuario() {
 
-    axios.get('https://api.github.com/users/' + inputElement.value + '/repos')
+    axios.get('https://api.github.com/users/diego3g/repos')
         .then(function(response) {
             renderRepositories(response.data);
         })
@@ -21,6 +22,10 @@ function procurarUsuario() {
 
 function renderRepositories(repos) {
 
-
+    for (const repo of repos) {
+        var li = document.createElement('li');
+        li.innerHTML = repo.name;
+        ulElement.appendChild(li);
+    }
 
 }
