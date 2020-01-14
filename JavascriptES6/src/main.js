9b9f55c52b362522926cb5390eed3c4496d7d21b
william xavier <william.10.xavier@hotmail.com>
2020-01-14 20:32:51 -0300

    finalizado a aplicação com ES6

--- JavascriptES6/src/main.js
+++ JavascriptES6/src/main.js
@@ -145,6 +145,8 @@ console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
 
 //#region EXERCICIO 1
 
+/*
+
 // import ClasseUsuario from './functions';
 // import { idade } from './functions';
 import Usuario, { idade as IdadeUsuario } from './functions';
@@ -152,8 +154,166 @@ import Usuario, { idade as IdadeUsuario } from './functions';
 Usuario.info();
 
 console.log(IdadeUsuario);
+*/
 
 //#endregion
 
 
 //#endregion
+
+
+
+
+//#region AULA ASYNC AWAIT
+
+/*
+const minhaPromisse = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000);
+})
+
+// primeiro modo de fazer
+// async function executaPromise() {
+//     console.log(await minhaPromisse());
+//     console.log(await minhaPromisse());
+//     console.log(await minhaPromisse());
+// }
+
+// modo com arrowfunction
+const executaPromise = async() => {
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+};
+
+executaPromise();
+*/
+//#endregion
+
+
+//#region AULA AXIOS
+
+/*
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username) {
+        try {
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response);
+        } catch (err) {
+            console.log('Erro na API');
+        }
+
+    }
+}
+
+Api.getUserInfo('Tuchtenhagen');
+Api.getUserInfo('Tuchtenhagentesteerrado');
+
+*/
+//#endregion
+
+
+
+
+
+import api from './api';
+
+
+class App {
+
+    constructor() {
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0)
+            return;
+
+        this.setLoading();
+
+        try {
+
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+            console.log(response);
+
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+
+            this.inputEl.value = '';
+
+            this.render();
+        } catch (err) {
+            alert('Repositorio não existe');
+
+            this.inputEl.value = '';
+        }
+
+        this.setLoading(false);
+
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+    }
+}
+
+new App();
