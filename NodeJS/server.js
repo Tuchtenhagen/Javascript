1a4b91cec228faeb6709920378ed9b21039c66bd
william xavier <william.10.xavier@hotmail.com>
2020-02-04 20:48:31 -0300

    Instalação mongodb e reestruturação de arquivos

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//iniciando o app
 const app = express();
 
-app.get('/', (req, res) => {
+// iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 
-    res.send("Hello World");
+requireDir('./src/models');
 
-});
+// const Product = mongoose.model('Product');
+
+
+
+// rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
